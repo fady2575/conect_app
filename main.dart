e2d06376ec64938e0078edbd30c4d155449c6a5c
fady2575 <fodafady746@gmail.com>
2025-06-29 15:31:51 +0300

    main.dart

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'package:conect_app/SplashScreen.dart';
+import 'package:flutter/material.dart';
+
+import 'screns/home_screen_modified.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
